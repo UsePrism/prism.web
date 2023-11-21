e2df9abfbc2794dc3f75911286ce098c5c379d60
Oghenemaro Okolosio <Oghenemaro.Okolosio@stanbicibtc.com>
2023-11-21 22:21:28 +0100

    completed home cta

--- src/views/public/Home.tsx
+++ src/views/public/Home.tsx
@@ -2,7 +2,7 @@ import { lightAccent } from "core/consts/images";
 
 const Home = () => {
   return (
-    <div>
+    <div className="m-[0px] bg-black">
       <section className="h-[80vh] bg-black">
         <div className="relative mx-auto flex h-full w-11/12 justify-end overflow-hidden md:w-4/5">
           <div className="absolute left-0 top-0 flex h-full w-full flex-col justify-center text-white md:w-4/5 lg:w-3/5">
@@ -23,7 +23,7 @@ const Home = () => {
                 name="search"
                 id="search"
                 placeholder="what business are you looking for?"
-                className="flex h-12 w-9/12 items-center justify-center rounded-l-[5px] border border-[1px] border-line bg-brand-white bg-white/0 p-3 text-sm outline-none"
+                className="flex h-12 w-9/12 items-center justify-center rounded-l-[5px] border border-[1px] border-line bg-brand-white p-3 text-sm outline-none"
               />
               <button className="w-3/12 rounded-r-[5px] bg-brand px-5 py-3">
                 Search
