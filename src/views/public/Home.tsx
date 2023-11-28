b9eb14cbf2c47d22672084124d66e6fd3253b11c
Oghenemaro Okolosio <Oghenemaro.Okolosio@stanbicibtc.com>
2023-11-29 00:29:41 +0100

    completed home page

--- src/views/public/Home.tsx
+++ src/views/public/Home.tsx
@@ -1,8 +1,11 @@
+/* eslint-disable no-template-curly-in-string */
 import { lightAccent } from "core/consts/images";
 import { useState } from "react";
 import user from "assets/img/user.svg";
-import { reverse } from "dns";
-import { renderStars } from "core/services/helpers";
+import { getCatgories, renderStars } from "core/services/helpers";
+import { Link } from "react-router-dom";
+import { btn } from "core/consts/styling";
+import bgbrandPlain from "assets/img/bg-brand-plain.svg";
 
 const Home = () => {
   const [reviews, setReviews] = useState([
@@ -49,6 +52,8 @@ const Home = () => {
         "Innovative payment solutions tailored for the Nigerian market. GTPay is a great choice for businesses that want to offer their customers a variety of payment options.",
     },
   ]);
+
+  const categories = getCatgories();
 
   return (
     <div className="m-[0px]">
@@ -105,6 +110,109 @@ const Home = () => {
                 <p>{review?.review}</p>
               </div>
             ))}
+        </div>
+      </section>
+      <section className="bg-[#9747FF26]">
+        <div className="mx-auto flex w-11/12 flex-col items-center justify-center gap-5 overflow-hidden py-[60px] sm:flex-row md:w-4/5">
+          <h3 className="text-[24px] font-[600]">Are you a business?</h3>
+          <Link
+            to="/auth/signup"
+            className={`${btn} border-1 border border-brand bg-brand !text-[18px]  text-white`}
+          >
+            Get Started
+          </Link>
+        </div>
+      </section>
+      <section className="mx-auto w-11/12 overflow-hidden py-[60px] md:w-4/5">
+        <div className="mb-[28px] w-full text-center">
+          <h3 className="w-full text-[24px] text-[32px] font-[600]">
+            Browse through Categories
+          </h3>
+        </div>
+        <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4">
+          {categories &&
+            categories?.length > 0 &&
+            categories.map((category: any, index: number) => (
+              <div
+                key={index}
+                className="flex items-center gap-5 bg-[#F9FAFB] p-5"
+              >
+                <img src={category?.icon} alt="" />
+                <p className="text-[14px] font-[500]">{category?.name}</p>
+              </div>
+            ))}
+        </div>
+        <div className="my-[34px] flex w-full justify-center">
+          <Link
+            to="/categories"
+            className={`${btn} border-1 border border-[#F9F5FF] bg-[#F9F5FF] !text-[18px] text-brand`}
+          >
+            View more
+          </Link>
+        </div>
+      </section>
+
+      <section className="bg-brandImg relative h-[85vh]">
+        <div className="absolute left-0 top-0 z-10 h-full w-full bg-[#220372] opacity-70"></div>
+        <div className="absolute left-0 top-0 z-50 h-full w-full">
+          <div className="mx-auto flex h-full w-11/12 flex-col justify-center overflow-hidden py-[60px] md:w-4/5">
+            <div className="w-full text-white md:w-2/3 lg:w-1/2">
+              <h5 className="mb-[24px] text-[32px] font-[600]">
+                Great? Poor? or a bit <br /> meh? Let us know
+              </h5>
+              <p className="md:w-2/2 mb-16 w-full">
+                Your reviews help other people to shop safely and confidently,
+                without the fear of buying poor quality products and services,
+                or being defrauded.
+              </p>
+              <Link
+                to="/reviews"
+                className={`${btn} border-1   w-auto border border-[#F9F5FF] bg-[#F9F5FF] !py-[12px] !text-[18px] text-brand md:w-2/3`}
+              >
+                Review a Business
+              </Link>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="relative h-[85vh] bg-white">
+        <div className="absolute left-0 top-0 z-10 h-full w-full bg-white opacity-70"></div>
+        <div className="absolute left-0 top-0 z-50 h-full w-full">
+          <div className="mx-auto flex h-full w-11/12 flex-col items-end justify-center overflow-hidden py-[60px] md:w-4/5">
+            <div className="w-full text-black md:w-4/5 lg:w-1/2">
+              <h5 className="mb-[24px] text-[32px] font-[600]">
+                Read authentic reviews <br /> from real people
+              </h5>
+              <p className="mb-16 w-full">
+                We spotlight businesses by pooling the feedback of customers to
+                show you the experiences of people who have shopped there.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="bg-yellowImg relative h-[85vh]">
+        <div className="absolute left-0 top-0 z-10 h-full w-full bg-[#C56A00] opacity-[.86]"></div>
+        <div className="absolute left-0 top-0 z-50 h-full w-full">
+          <div className="mx-auto flex h-full w-11/12 flex-col justify-center overflow-hidden py-[60px] md:w-4/5">
+            <div className="w-full text-white md:w-2/3 lg:w-1/2">
+              <h5 className="mb-[24px] text-[32px] font-[600]">
+                Beat the suspicion with <br /> feedback from customers
+              </h5>
+              <p className="md:w-2/2 mb-16 w-full">
+                Collect feedback from your customers and showcase on your
+                website or social media to help new customers confidently shop
+                from you.
+              </p>
+              <Link
+                to="/auth/signup"
+                className={`${btn} border-1   w-auto border border-[#001219] bg-[#001219] !py-[12px] !text-[18px] text-brand-white lg:w-2/3`}
+              >
+                Create your Business
+              </Link>
+            </div>
+          </div>
         </div>
       </section>
     </div>
